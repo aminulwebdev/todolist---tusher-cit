ed58df47fb5fbabc8c9eb858310ea25533f464db
Web-Tushar <tusharimranvip895@gmail.com>
2024-02-13 14:21:37 +0600

    project setup & todo list add

---
+++ script.js
@@ -0,0 +1,58 @@
+let inputbox = document.querySelector(".inputbox");
+let addbtn = document.querySelector(".addbtn");
+let error = document.querySelector(".error");
+let main = document.querySelector(".main");
+
+let storetodo = [];
+let todoindex 
+
+
+addbtn.addEventListener("click",function(){
+     if(inputbox.value !== "" && addbtn.innerHTML == "Add"){
+          error.innerHTML = ""
+          storetodo.push(inputbox.value)
+          inputbox.value = ""
+          todo()  
+
+     }
+     else if(inputbox.value !== "" && addbtn.innerHTML == "Update"){
+          storetodo[todoindex] = inputbox.value
+          inputbox.value = ""
+          addbtn.innerHTML = "Add"
+          todo() 
+
+     }
+
+     else{
+          error.innerHTML = "please enter a todo"
+     }
+})
+
+function todo(){
+     main.innerHTML = ""
+     storetodo.map((item,index)=>{
+          
+          main.innerHTML += `<li>${item}  <button class="deletebtn">delete</button> <button class="editbtn">Edit</button></li>`
+
+     })
+     let editbtn = document.querySelectorAll(".editbtn");
+     let editbtnarry = Array.from(editbtn);
+     editbtnarry.map((edititem,editindex)=>{
+          edititem.addEventListener("click",function(){
+               addbtn.innerHTML = "update"
+          inputbox.value = storetodo[editindex]
+          todoindex = todoindex
+
+               // console.log(edititem,editindex);
+          })
+     })
+     let deletebtn = querySelectorAll(".deletebtn")
+     let deletebtnArr = Arry.from("deletebtn")
+     deletebtnArr.map((deleteitem,deleteindex)=>{
+          deleteitem.addEventListener("click",function(){
+               storetodo.splice(deleteindex,1)
+               todo()
+          })
+     })
+
+}
